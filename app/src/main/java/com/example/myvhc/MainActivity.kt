8489bd6baad4355223302f0467f27fc10ab2445b
tnnHiu <trinhnguyennhathieu@gmail.com>
2023-03-26 22:41:28 +0700

    view Binding 26/03/2023

--- app/src/main/java/com/example/myvhc/MainActivity.kt
+++ app/src/main/java/com/example/myvhc/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myvhc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.myvhc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnTest.setOnClickListener {
+            Toast.makeText(this,  "Hello",Toast.LENGTH_LONG).show()
+        }
+
     }
 }
