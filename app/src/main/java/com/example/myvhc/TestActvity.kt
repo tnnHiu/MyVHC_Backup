f95244d2a5e0849f304393a19721ef45200eecbd
tnnHiu <trinhnguyennhathieu@gmail.com>
2023-03-26 23:01:14 +0700

    button back testActivity 26/03/2023

--- app/src/main/java/com/example/myvhc/TestActvity.kt
+++ app/src/main/java/com/example/myvhc/TestActvity.kt
@@ -1,11 +1,22 @@
 package com.example.myvhc
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.myvhc.databinding.ActivityTestActvityBinding
 
 class TestActvity : AppCompatActivity() {
+    private lateinit var binding: ActivityTestActvityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test_actvity)
+        binding = ActivityTestActvityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnback.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+            Toast.makeText(this, "Quay lại rồi con chó",Toast.LENGTH_LONG).show()
+        }
+
     }
 }
