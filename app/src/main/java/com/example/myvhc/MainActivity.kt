ca3433b4fca969ea98030b4b86561146ef9e9577
nguye <antoin2901@gmail.com>
2023-04-11 16:14:39 +0700

    dua code len 11/04/2023

--- app/src/main/java/com/example/myvhc/MainActivity.kt
+++ app/src/main/java/com/example/myvhc/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.example.myvhc
 
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myvhc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
 
     }
