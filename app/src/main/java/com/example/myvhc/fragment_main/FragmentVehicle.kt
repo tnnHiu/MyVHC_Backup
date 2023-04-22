f3ba6f2420302f01fec6ecab0217f5afd0e128f0
nguye <antoin2901@gmail.com>
2023-04-22 13:22:14 +0700

    thêm layout title maps 22/4/2023

--- app/src/main/java/com/example/myvhc/fragment_main/FragmentVehicle.kt
+++ app/src/main/java/com/example/myvhc/fragment_main/FragmentVehicle.kt
@@ -1,20 +1,31 @@
 package com.example.myvhc.fragment_main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.myvhc.R
+import com.example.myvhc.databinding.FragmentVehicleBinding
+import com.example.myvhc.myVHCDetailActivity.MyMotorDetailActivity
 
 
 class FragmentVehicle : Fragment() {
+
+    private lateinit var binding: FragmentVehicleBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vehicle, container, false)
+        binding = FragmentVehicleBinding.inflate(layoutInflater, container, false)
+
+        binding.motor.setOnClickListener {
+            startActivity(Intent(context, MyMotorDetailActivity::class.java))
+        }
+
+        return binding.root
     }
 
 }
