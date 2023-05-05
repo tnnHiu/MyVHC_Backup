253a39261ff038bc628438589720d1f754a52c94
nguye <antoin2901@gmail.com>
2023-05-05 18:27:44 +0700

    them layout agency detail & service activity 05/05/2023

--- app/src/main/java/com/example/myvhc/fragment/FragmentHome.kt
+++ app/src/main/java/com/example/myvhc/fragment/FragmentHome.kt
@@ -1,11 +1,13 @@
 package com.example.myvhc.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.myvhc.databinding.FragmentHomeBinding
+import com.example.myvhc.serviceActivity.AgencyDetailActivity
 
 class FragmentHome : Fragment() {
     // TODO: Rename and change types of parameters
@@ -16,6 +18,10 @@ class FragmentHome : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.test.setOnClickListener{
+            startActivity(Intent(context, AgencyDetailActivity::class.java))
+        }
 
         return binding.root
     }
