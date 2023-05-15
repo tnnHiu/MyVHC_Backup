0a2466113f894d6aa77eec6992041b875ad778d3
trinh <trinhnguyennhathieu@gmail.com>
2023-05-15 16:11:39 +0700

    commit

---
+++ app/src/main/java/com/example/myvhc/admin/adapter/AdminVehicleAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.myvhc.admin.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.example.myvhc.databinding.ItemAdminVehicleBinding
+import com.example.myvhc.models.Vehicle
+
+class AdminVehicleAdapter(
+    private val data: ArrayList<Vehicle>
+) : RecyclerView.Adapter<AdminVehicleAdapter.ViewHolder>() {
+//    private lateinit var mListener: OnItemClickListener
+//
+//    interface OnItemClickListener {
+//        fun onItemClick(position: Int)
+//    }
+
+    inner class ViewHolder(
+        val binding: ItemAdminVehicleBinding,
+//        clickListener: OnItemClickListener
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun loadImg(): RequestManager {
+            return Glide.with(itemView)
+        }
+
+//        init {
+//            itemView.setOnClickListener {
+//                clickListener.onItemClick(adapterPosition)
+//            }
+//        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = ItemAdminVehicleBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return ViewHolder(view,
+//            mListener
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.loadImg().load(data[position].vehicleImg).into((holder.binding.imgVehicle))
+        holder.binding.vehicleChassisNum.text = data[position].vehicleChassisNum
+    }
+}
--- app/src/main/java/com/example/myvhc/admin/fragment/AdminVehicleFragment.kt
+++ app/src/main/java/com/example/myvhc/admin/fragment/AdminVehicleFragment.kt
@@ -1,28 +1,75 @@
 package com.example.myvhc.admin.fragment
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myvhc.admin.adapter.AdminVehicleAdapter
 import com.example.myvhc.admin.bottom_sheet.AddVehicleSheetFragment
 import com.example.myvhc.databinding.FragmentAdminVehicleBinding
+import com.example.myvhc.models.Vehicle
+import com.example.myvhc.viewmodels.VehicleViewModel
 
 class AdminVehicleFragment : Fragment() {
 
     private lateinit var binding: FragmentAdminVehicleBinding
+    private lateinit var vehicleVM: VehicleViewModel
+    private lateinit var mAdapter: AdminVehicleAdapter
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         binding = FragmentAdminVehicleBinding.inflate(layoutInflater, container, false)
 
         binding.btnAdd.setOnClickListener {
             AddVehicleSheetFragment().show(childFragmentManager, "newTaskTag")
         }
-
+        getData()
         return binding.root
     }
+
+    override fun onResume() {
+        super.onResume()
+        getData()
+
+    }
+
+    private fun getData() {
+        vehicleVM = ViewModelProvider(this)[VehicleViewModel::class.java]
+        vehicleVM.vListSize.observe(viewLifecycleOwner, Observer {
+            val vData = vehicleVM.vList.value!!
+            removeDuplicatesByChassisNumber(vData)
+            setupRecyclerView(vData)
+            Log.i("checkData", vData.toString())
+        })
+    }
+
+    private fun setupRecyclerView(vData: ArrayList<Vehicle>) {
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        mAdapter = AdminVehicleAdapter(vData)
+        recyclerView.adapter = mAdapter
+
+    }
+
+
+    private fun removeDuplicatesByChassisNumber(list: ArrayList<Vehicle>) {
+        val set = HashSet<String>()
+        val iterator = list.iterator()
+        while (iterator.hasNext()) {
+            val vehicle = iterator.next()
+            if (!vehicle.vehicleChassisNum?.let { set.add(it) }!!) {
+                iterator.remove()
+            }
+        }
+    }
+
 
 }
